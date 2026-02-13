e9dbd09012313b8f26bfa5798e429b769f366976
Mehdi Seifi <mehdiseifi@gmail.com>
2026-02-13 13:47:52 +0100

    updated dynamic versioning

--- src/my_exes/__init__.py
+++ src/my_exes/__init__.py
@@ -1,5 +1,12 @@
+from importlib.metadata import PackageNotFoundError, version
+
 from .csv_logger import CSVLogger
 from .my_ex import MyEx
 
+
+try:
+    __version__ = version("my-exes")
+except PackageNotFoundError:
+    __version__ = "uninstalled"
 
 __all__ = ["CSVLogger", "MyEx"]
